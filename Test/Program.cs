88c53b6eaf609cd8b606c0f3f671cd5c8f09335e
Jett Yuen <jettyuen29@gmail.com>
2021-08-19 07:22:44 -0700

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            YettPubSub.Class1 testClass = new();
+            testClass.Main();
+        }
+    }
+}
